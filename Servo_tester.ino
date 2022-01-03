670ce8cebd1d047e661772f72a77d3308edaa5a4
KozhaAkhmet <80167990+KozhaAkhmet@users.noreply.github.com>
2022-01-03 20:59:11 +0300

    Remake

--- Servo_tester.ino
+++ Servo_tester.ino
@@ -1,6 +1,4 @@
 #include <stdlib.h>
-#include <SPI.h>
-#include <Wire.h>
 #include <Servo.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -42,65 +40,48 @@ void setup() {
 
 void loop() {
   int *data = (int*)malloc(4*sizeof(int));
+  
   data[0]=analogRead(pot_pin1);
   data[1]=analogRead(pot_pin2);
   data[2]=map(analogRead(pot_pin1),0,669,0,180);
   data[3]=map(analogRead(pot_pin2),0,1023,270,0);
-/*
-  int *val1=(int*)malloc(sizeof(int));
-  int *val2=(int*)malloc(sizeof(int));
-  *val1=analogRead(pot_pin1);
-  *val2=analogRead(pot_pin2);
-  
-  input_angles1= map(analogRead(pot_pin1),0,751,0,180);
-  input_angles2= map(analogRead(pot_pin2),0,751,0,180);
- */
-  input_micro = analogRead(A1);
-  
+ 
   servo.write(data[3]);
-  //myservo.writeMicroseconds(map(inputmicro,0,1023,0,3000));
 
   serialLogDetails(data);
   oledLogDetailes(data);
+  
   free(data);
   delay(100);
 }
 
 void oledLogDetailes(int *data ){
   display.clearDisplay();
+  
   display.setTextSize(1); 
   display.setTextColor(SSD1306_WHITE);
   
   display.setCursor(0,0);
   display.print(F("Pot1:"));
   display.print(data[0]);
-  
+
   display.setCursor(0,8);
-  display.print(F("Pot2:"));
-  display.print(data[1]);
-  
-  display.setCursor(0,16);
-  display.print(F("Pot1 Ang:"));
+  display.print(F("Ang:"));
   display.print(data[2]);
   
-  display.setCursor(0,24);
-  display.print(F("Pot2 Ang:"));
-  display.print(data[3]);
+  display.setCursor(30,16);
+  display.print(F("Dif:"));
+  display.print(data[3]-data[2]);  //difference between pot angles
   //2nd coulomb
-  /*display.setCursor(75,0);
-  display.print(F("Dif:"));
-  display.print(data[1]-data[0]);         //difference between pot values
+  display.setCursor(70,0);
+  display.print(F("Pot2:"));
+  display.print(data[1]);
 
-  display.setCursor(75,8);             
-  display.print(F("Per:"));
-  display.print(int abs((data[1]-data[0])/10.23)); //mistake persentage between values
-  display.print(F("%"));*/
-
-  display.setCursor(75,16);
-  display.print(F("Dif:"));
-  display.print(data[3]-data[2]);         //difference between pot angles
-
-  display.setCursor(75,24);             
+  display.setCursor(70,8);
+  display.print(F("Ang:"));
+  display.print(data[3]);
+           
+  display.setCursor(30,24);             
   display.print(F("Per:"));
   display.print((5* (data[3]-data[2]))/9); //mistake persentage between angles
   display.print(F("%"));
